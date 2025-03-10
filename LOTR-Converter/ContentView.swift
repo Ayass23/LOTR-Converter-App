123da5034323a863b568b1d6e2f2ad1ceacb1cdb
Ayass23 <andreasmargono.23@gmail.com>
2025-03-10 21:23:45 +0800

    Update View

--- LOTR-Converter/ContentView.swift
+++ LOTR-Converter/ContentView.swift
@@ -8,21 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var showExchangeModal = false
+    @State var leftValue = ""
+    @State var rightValue = ""
     var body: some View {
         ZStack{
             //Background Image
             Image(.background).resizable().ignoresSafeArea()
             VStack{
-                Image(.prancingpony).resizable().scaledToFit().frame(height: 200)
-                Text("Currency Exchange").font(.largeTitle).foregroundStyle(.white)
                 HStack{
-                    Image(.silverpenny).resizable().scaledToFit().frame(width: 50, height: 30)
-                    Text("Silver Pieces").font(.headline).foregroundStyle(.white)
+                    VStack{
+                        Image(.prancingpony).resizable().scaledToFit().frame(height: 200)
+                        Text("Currency Exchange").font(.largeTitle).foregroundStyle(.white)
+                        HStack{
+                            VStack{
+                                HStack{
+                                    Image(.silverpenny).resizable().scaledToFit().frame(height: 33)
+                                    Text("Silver Pieces").font(.headline).foregroundStyle(.white)
+                                }.padding(.bottom, -5)
+                                TextField("Amount", text: $leftValue).textFieldStyle(.roundedBorder)
+                            }
+                            Image(systemName: "equal").font(.largeTitle).foregroundStyle(.white).symbolEffect(.pulse)
+                            
+                            VStack{
+                                HStack{
+                                    Text("Gold Piece").font(.headline).foregroundStyle(.white)
+                                    Image(.goldpiece).resizable().scaledToFit().frame(height: 33)
+                                }.padding(.bottom, -5)
+                                TextField("Amount", text: $rightValue).textFieldStyle(.roundedBorder)
+                            }
+                        }.padding().background(.black.opacity(0.5 )).clipShape(.capsule)
+                        Spacer()
+                        HStack{
+                            Spacer()
+                            Button{
+                                showExchangeModal.toggle()
+                            }label: {
+                                Image(systemName: "info.circle.fill").font(.largeTitle).foregroundStyle(.white)
+                            }.padding(.trailing)
+                        }
+                    }.border(.blue)
                 }
-                
             }
-            Image(systemName: "equal").font(.headline).foregroundStyle(.white)
-            
         }
     }
 }
